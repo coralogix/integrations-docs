185bc72217536a1b7391482a9d326b5d19f3598b
Eldar Aliiev <bc.zim.green@gmail.com>
2020-04-30 20:15:47 +0300

    Multiinstances feature added

--- integrations/fluent-bit/plugin/out_coralogix.go
+++ integrations/fluent-bit/plugin/out_coralogix.go
@@ -21,41 +21,25 @@ import (
 // Initialize connection details
 var endpoint string = "logstashserver.coralogix.com:5044"
 
-// Initialize output parameters
-var (
-    private_key         string
-    company_id          string
-    app_name            string
-    sub_name            string
-    app_name_key        string
-    sub_name_key        string
-    time_key            string
-    log_key             string
-    host_key            string
-    debug               string
-    compression         string
-    compression_level   int
-)
-
 //export FLBPluginRegister
-func FLBPluginRegister(ctx unsafe.Pointer) int {
-    return output.FLBPluginRegister(ctx, "coralogix", "Send output to Coralogix")
+func FLBPluginRegister(def unsafe.Pointer) int {
+    return output.FLBPluginRegister(def, "coralogix", "Send output to Coralogix")
 }
 
 //export FLBPluginInit
-func FLBPluginInit(ctx unsafe.Pointer) int {
+func FLBPluginInit(plugin unsafe.Pointer) int {
     // Get output parameters
-    private_key = output.FLBPluginConfigKey(ctx, "Private_Key")
-    company_id = output.FLBPluginConfigKey(ctx, "Company_Id")
-    app_name = output.FLBPluginConfigKey(ctx, "App_Name")
-    sub_name = output.FLBPluginConfigKey(ctx, "Sub_Name")
-    app_name_key = output.FLBPluginConfigKey(ctx, "App_Name_Key")
-    sub_name_key = output.FLBPluginConfigKey(ctx, "Sub_Name_Key")
-    time_key = output.FLBPluginConfigKey(ctx, "Time_Key")
-    log_key = output.FLBPluginConfigKey(ctx, "Log_Key")
-    host_key = output.FLBPluginConfigKey(ctx, "Host_Key")
-    compression = output.FLBPluginConfigKey(ctx, "Compression")
-    debug = output.FLBPluginConfigKey(ctx, "Debug")
+    private_key := output.FLBPluginConfigKey(plugin, "Private_Key")
+    company_id := output.FLBPluginConfigKey(plugin, "Company_Id")
+    app_name := output.FLBPluginConfigKey(plugin, "App_Name")
+    sub_name := output.FLBPluginConfigKey(plugin, "Sub_Name")
+    app_name_key := output.FLBPluginConfigKey(plugin, "App_Name_Key")
+    sub_name_key := output.FLBPluginConfigKey(plugin, "Sub_Name_Key")
+    time_key := output.FLBPluginConfigKey(plugin, "Time_Key")
+    log_key := output.FLBPluginConfigKey(plugin, "Log_Key")
+    host_key := output.FLBPluginConfigKey(plugin, "Host_Key")
+    compression := output.FLBPluginConfigKey(plugin, "Compression")
+    debug := output.FLBPluginConfigKey(plugin, "Debug")
 
     // Debug output
     log.SetPrefix("[CORALOGIX] ")
@@ -93,16 +77,34 @@ func FLBPluginInit(ctx unsafe.Pointer) int {
     }
 
     // Check compression level
-    if compression_level, err := strconv.ParseInt(compression, 10, 64); err != nil || compression_level < 0 || compression_level > 9 {
-        compression_level = 3
+    if compression_integer, err := strconv.ParseInt(compression, 10, 64); err != nil || compression_integer < 0 || compression_integer > 9 {
+        compression = "3"
     }
 
-    log.Printf("The Application Name %s and Subsystem Name %s from the Fluent-Bit, has started to send data.", app_name, sub_name)
+    if debug == "On" {
+        log.Printf("The Application Name %s and Subsystem Name %s from the Fluent-Bit, has started to send data.", app_name, sub_name)
+    }
+
+    // Pass output configuration to context
+    output.FLBPluginSetContext(plugin, map[string]string{
+        "private_key": private_key,
+        "company_id": company_id,
+        "app_name": app_name,
+        "sub_name": sub_name,
+        "app_name_key": app_name_key,
+        "sub_name_key": sub_name_key,
+        "time_key": time_key,
+        "log_key": log_key,
+        "host_key": host_key,
+        "debug": debug,
+        "compression": compression,
+    })
+
     return output.FLB_OK
 }
 
-//export FLBPluginFlush
-func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
+//export FLBPluginFlushCtx
+func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int {
     var ret int
     var batch []interface{}
     var record map[interface{}]interface{}
@@ -116,8 +118,14 @@ func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
     // Create Fluent-Bit decoder
     dec := output.NewDecoder(data, int(length))
 
+    // Get plugin instance configuration
+    config := output.FLBPluginGetContext(ctx).(map[string]string)
+
+    // Get compression level
+    compression, err := strconv.Atoi(config["compression"])
+
     // Send logs batch to Logstash
-    connection, err := v2.SyncDial(endpoint, v2.CompressionLevel(compression_level), v2.Timeout(30 * time.Second))
+    connection, err := v2.SyncDial(endpoint, v2.CompressionLevel(compression), v2.Timeout(30 * time.Second))
     if err != nil {
         log.Printf(" ERROR: unable connect to %s: %v\n", endpoint, err)
         return output.FLB_ERROR
@@ -140,21 +148,21 @@ func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
 
         // Build logs batch
         batch = append(batch, map[string]interface{}{
-            "@timestamp": extractField(json_record, time_key, time.Now().Format(time.RFC3339)),
+            "@timestamp": extractField(json_record, config["time_key"], time.Now().Format(time.RFC3339)),
             "type": "fluent-bit",
             "beat": map[string]interface{}{
-                "hostname": extractField(json_record, host_key, hostname),
+                "hostname": extractField(json_record, config["host_key"], hostname),
             },
-            "message": extractField(json_record, log_key, json_record),
+            "message": extractField(json_record, config["log_key"], json_record),
             "fields": map[string]interface{}{
-                "PRIVATE_KEY": private_key,
-                "COMPANY_ID": company_id,
-                "APP_NAME": extractField(json_record, app_name_key, app_name),
-                "SUB_SYSTEM": extractField(json_record, sub_name_key, sub_name),
+                "PRIVATE_KEY": config["private_key"],
+                "COMPANY_ID": config["company_id"],
+                "APP_NAME": extractField(json_record, config["app_name_key"], config["app_name"]),
+                "SUB_SYSTEM": extractField(json_record, config["sub_name_key"], config["sub_name"]),
             },
         })
 
-        if debug == "On" {
+        if config["debug"] == "On" {
             log.Printf(" INFO: Sending %d records to %s...\n", len(batch), endpoint)
         }
         _, err = connection.Send(batch)
